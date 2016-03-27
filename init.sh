3af536dc48fb38a68d3f4cf101a24001bec5672b
Scyptnex <scyptnex@tzeidn.com>
2016-03-27 23:42:03 +1100

    taking over the bin

--- init.sh
+++ init.sh
@@ -19,14 +19,28 @@ function githubget(){
 
 which git >/dev/null 2>/dev/null || (echo "Install git" >&2 && exit 1)
 which openssl >/dev/null 2>/dev/null || (echo "Install openssl" >&2 && exit 1)
+REPO_DIR="bootstrap"
 TMP_DIR=`mktemp -d`
 RSA_FILE="rsa_2048.aes-256-cbc.enc"
-githubget ssh "sshup.sh"
-githubget ssh "$RSA_FILE"
-githubget bin "crypt"
-chmod a+x "$TMP_DIR/sshup.sh"
-chmod a+x "$TMP_DIR/crypt"
-PATH="$PATH:$TMP_DIR" $TMP_DIR/sshup.sh "$TMP_DIR/$RSA_FILE"
-rm -rf $TMP_DIR
-git clone git@github.com:scyptnex/bootstrap.git
+# githubget ssh "sshup.sh"
+# githubget ssh "$RSA_FILE"
+# githubget bin "crypt"
+# chmod a+x "$TMP_DIR/sshup.sh"
+# chmod a+x "$TMP_DIR/crypt"
+# PATH="$PATH:$TMP_DIR" $TMP_DIR/sshup.sh "$TMP_DIR/$RSA_FILE"
+# rm -rf $TMP_DIR
+# git clone git@github.com:scyptnex/bootstrap.git $REPO_DIR
+
+# linking configurations
+for CFG in $REPO_DIR/config/*; do
+    if [[ $CFG =~ .*bashrc$ ]] && [ -f $HOME/.bashrc ]; then
+        # put the config's bashrc in the current bashrc if it exists and is a file
+        sed -i -e '/^\..*bashrc$/d' $HOME/.bashrc
+        echo ". `readlink -f $CFG`" >> $HOME/.bashrc
+    else
+        # remove anythink that is not the proper config and link the config
+        rm -f ~/.${CFG#$REPO_DIR/config/}
+        ln -s $(readlink -f "$CFG") ~/.${CFG#$REPO_DIR/config/}
+    fi
+done
 
