1b53c11d1f48486deaaf639c0c4beb3b693f7c68
Scyptnex <nic.h.misc@gmail.com>
2017-04-19 17:35:38 +1000

    i3 config

--- etc/.xmonad/xmonad.hs
+++ etc/.xmonad/xmonad.hs
@@ -2,7 +2,9 @@ import XMonad
 import XMonad.Config.Xfce
 import XMonad.Hooks.SetWMName
 
-startupHook = setWMName "LG3D"
+startupHook = do
+        setWMName "LG3D"
+        spawn "~/.xmonad/xmonadrc"
  
 main = xmonad xfceConfig
         { modMask = mod4Mask }
