816c8538932389b4eaac2cd55e4cb13380bbfcdc
Scyptnex <nic.h.misc@gmail.com>
2020-06-24 19:53:01 +1000

    configure with stow

--- init/configure.sh
+++ init/configure.sh
@@ -12,20 +12,4 @@
 set -e
 set -u
 
-REPO_DIR=`readlink -f $(dirname $0)/..`
-
-# HACK1, forcibly remove gnupg configuration
-[ -f ~/.gnupg/gpg.conf ] && rm ~/.gnupg/gpg.conf
-
-for IN_F in `find $REPO_DIR/dotfiles/ -type f`; do
-    OUT="${IN_F#*${REPO_DIR}/dotfiles/}"
-    IN=$(readlink -f "$IN_F")
-    mkdir -p $(dirname ~/$OUT)
-
-    # if the file exists, remove symlinks, backup others
-    [ -h ~/$OUT ] && rm ~/$OUT
-    [ -f ~/$OUT ] && mv ~/${OUT}{,_BACKUP}
-
-    echo "$IN ~/$OUT"
-    ln -s "$IN" ~/$OUT
-done
+stow -Rv --dir=${HOME}/project/bootstrap/dotfiles/ --target=${HOME} --no-folding .
